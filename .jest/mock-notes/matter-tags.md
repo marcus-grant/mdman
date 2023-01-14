76f41bee74ff875e91565b7a0d76287584535de4
Marcus Grant <marcusfg@gmail.com>
2023-01-14 18:10:55 +0100

    Partial frontmatter-file module
    * Will continue as part of issue #12
    * Adds the frontmatter module with some tests
    * However, for typing through JSDOC and TS to work...
      * Needs more work on the links in the file.
    * Abandoning for now because I need this to actually work first

---
+++ .jest/mock-notes/matter-tags.md
@@ -0,0 +1,7 @@
+---
+tags: [a, b, c, d, e, f, g]
+---
+# Markdown File with Tags
+
+There's a list of tags in the YAML frontmatter.
+Should affect how stringifying happens depending on `flowLevel`.
---
+++ __tests__/frontmatter-file.test.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+const FMFile = require('../lib/frontmatter-file');
+const {
+  TESTING_DIR_PATH,
+} = require('../.jest/mocks');
+
+const testDirAbsPath = (fname) => path.join(TESTING_DIR_PATH, fname);
+
+describe('lib/front-matterfile.Constructor', () => {
+  let tagMatterFile;
+  beforeAll(() => {
+    // tagMatterFile = fs.readFileSync(testDirAbsPath('modified-matter.md'));
+    tagMatterFile = testDirAbsPath('matter-tags.md');
+  });
+  it('test', () => {
+    const fmf = FMFile(tagMatterFile);
+    expect(fmf.stringify()).toEqual(`---
+tags: [a, b, c, d, e, f, g]
+---
+# Markdown File with Tags
+
+There's a list of tags in the YAML frontmatter.
+Should affect how stringifying happens depending on \`flowLevel\`.
+`);
+  });
+});
+
+// describe('lib/frontmatter-file.getModifiedDate()', () => {
+//   let modifiedMatterFile;
+//   beforeAll(() => {
+//     modifiedMatterFile = FMFile(testDirAbsPath('matter-modified.md'));
+//   });
+//   it('returns a date', () => {
+//     expect(modifiedMatterFile.getModifiedDate()).toEqual(new Date());
+//   });
+// });
