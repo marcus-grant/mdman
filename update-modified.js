678023e50a013c77cec70e3160fd022b8c701c3e
Marcus Grant <marcusfg@gmail.com>
2023-01-27 18:33:37 +0100

    Remove old root scripts

--- update-modified.js
+++
@@ -1,9 +0,0 @@
-const { listAllMdFiles } = require('./lib/file-management');
-const { readFileModifiedMatter } = require('./lib/front-matter');
-
-
-const main = () => {
-  // script running code
-};
-
-if (require.main === module) { main(); }
